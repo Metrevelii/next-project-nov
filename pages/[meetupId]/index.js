5380135600c5ddb32ee8c9dd45d976d90576dcad
Zauri Metreveli <72525534+Metrevelii@users.noreply.github.com>
2022-11-16 15:30:02 +0400

    added cstom components 334

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg"
+      title="First Meetup"
+      address="Some street 5, Some City"
+      description="This is 1st meetup"
+    />
+  );
+}
+
+export default MeetupDetails;
